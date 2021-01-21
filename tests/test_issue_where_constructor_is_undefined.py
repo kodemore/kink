97aebd29ddb15be551e136418382e93f5199a62b
dkraczkowski <dawid.kraczkowski@gmail.com>
2021-01-21 09:31:22 +0100

    #4 - Fix testcase

--- tests/test_issue_where_constructor_is_undefined.py
+++ tests/test_issue_where_constructor_is_undefined.py
@@ -1,7 +1,6 @@
-from typing import Protocol
+from typing_extensions import Protocol
 
-from kink import di
-from kink import inject
+from kink import di, inject
 
 
 class ExampleProtocol(Protocol):
