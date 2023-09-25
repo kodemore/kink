2e84bd21256b251e4e700f122804c00a41053d60
Dawid Kraczkowski <dawid.kraczkowski@gmail.com>
2023-09-25 09:21:32 +0200

    Add test coverage for optional auto-resoving wo value

--- tests/test_optional_support.py
+++ tests/test_optional_support.py
@@ -19,3 +19,17 @@ def test_can_inject_optional_support_with_present_value() -> None:
         return ""
 
     assert inject_test() == "Hello world"
+
+
+def test_can_inject_optional_support_without_present_value() -> None:
+    class Message:
+        def __init__(self, value: str) -> None:
+            self.value = value
+
+    @inject()
+    def inject_test(message: Optional[Message] = None):
+        if message:
+            return message.value
+        return ""
+
+    assert inject_test() == ""
