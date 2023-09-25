9c8cebfda64366078c37f22bb2b8cb43336b7c7b
Dawid Kraczkowski <dawid.kraczkowski@gmail.com>
2023-09-25 09:20:36 +0200

    Add test coverage for optional auto-resolving

---
+++ tests/test_optional_support.py
@@ -0,0 +1,21 @@
+from typing import Optional
+
+from kink import di
+from kink import inject
+
+
+def test_can_inject_optional_support_with_present_value() -> None:
+
+    class Message:
+        def __init__(self, value: str) -> None:
+            self.value = value
+
+    di[Message] = Message("Hello world")
+
+    @inject()
+    def inject_test(message: Optional[Message] = None):
+        if message:
+            return message.value
+        return ""
+
+    assert inject_test() == "Hello world"
