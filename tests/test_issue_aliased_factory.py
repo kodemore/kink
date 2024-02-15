504cab78aa720604a2e2ecca3d74c09a1796746b
jarey <jose.angel.rey.linares@gmail.com>
2024-02-16 00:26:22 +0100

    chore: expand scope of test
    expand scope of test by also testing protocols

--- tests/test_issue_aliased_factory.py
+++ tests/test_issue_aliased_factory.py
@@ -1,16 +1,42 @@
+from abc import ABC
+from typing import Protocol
+
 from kink import inject, di
 
-class Repository:
-   pass
+
+class Repository(Protocol):
+    def __init__(self) -> None:
+        pass
+
 
 @inject(alias=Repository, use_factory=True)
 class PerInstanceRepository(Repository):
-   pass
+    pass
+
+
+@inject(alias=Repository)
+class Repository1(Repository):
+    pass
+
+
+@inject(alias=Repository)
+class Repository2(Repository):
+    repo: Repository1 = di[Repository1]
+
 
 @inject
 class Service:
-   def __init__(self, repository: Repository):
-      pass
-   
+    def __init__(self, repositories: list[Repository]):
+        self._repositories = repositories
+
+
 def test_can_inject_aliased_factory_services():
-   di[Service]
+    service: Service = di[Service]
+    assert service is not None
+    assert service._repositories is not None
+    assert len(service._repositories) == 3
+    repository: Repository = di[Repository2]
+    assert repository.repo is not None
+    repositories: list[Repository] = di[list[Repository]]
+    assert repositories is not None
+    assert len(repositories) == 3
